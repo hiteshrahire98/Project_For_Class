55956ce92c3a31dd5306a73f81632640bf07e7ff
hiteshahire98 <hituahire1998@gmail.com>
2022-09-29 16:11:05 +0530

    Add project files.

---
+++ Project_For_Class/Date.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_For_Class
+{
+    public class Date
+    {
+        private int day, year;
+
+        private string month;
+
+        // using method assign value to the member
+
+        public void AddValues(int d, string m, int y)
+
+        {
+
+            day = d;
+
+            month = m;
+
+            year = y;
+
+        }
+
+        public string GetValues()
+
+        {
+
+            return $"Date : {day}/{month}/{year}";  //Date : 29/Sept/2022
+
+        }
+
+    }
+}
---
+++ Project_For_Class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_For_Class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // create object of Date class
+
+            Date d1 = new Date(); // new allocate memory dynamically on the heap
+
+            Console.WriteLine("Enter day month &  year");
+
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            string m = Console.ReadLine();
+
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            d1.AddValues(d, m, y);
+
+            Console.WriteLine(d1.GetValues());
+
+            Date d2 = new Date();
+
+            d2.AddValues(10, "Jan", 2022);
+
+            Console.WriteLine(d2.GetValues());
+        }
+    }
+}
